48ca1a5d9c02f9bde46b45fcffbad0ccfba4777a
小波 <mcxiaobo1314@qq.com>
2017-03-29 14:49:00 +0800

    Update Container.php

--- lib/core/Container.php
+++ lib/core/Container.php
@@ -8,10 +8,22 @@
 class Container {
 
 	/**
-	 * 初始化保存变量
+	 * 注入的数据
 	 * @author wave
 	 */
 	public static $app = array();
+	
+	/**
+	 * 绑定的数据
+	 * @author wave
+	 */
+	private static $bind = array();
+	
+	/**
+	 * 绑定注入的前缀
+	 * @author wave
+	 */
+	private static $bindPrefix = 'bind_';
 
 
 	/**
@@ -24,13 +36,14 @@ class Container {
 	 */
 	public static function instace($class,$method = '',$params = array(),$prefix = ''){
 		$pclass = $prefix.$class;
-		if(empty(self::$app[$pclass]) && class_exists($class)){
+					
+		if(!isset(self::$app[$pclass]) && class_exists($class)){
 			self::$app[$pclass] =  new $class();
 		}
 		if($method != '' && self::$app[$pclass] instanceof $class){
 			return self::methodInstace(self::$app[$pclass],$method,$params);
 		}
-		if(empty(self::$app[$pclass])){
+		if(!isset(self::$app[$pclass])) {
 			throw new XiaoBoException($class."类不存在");
 		}
 	}
@@ -44,7 +57,7 @@ class Container {
 	 * @author wave
 	 */
 	public static function staticInstace($class,$method = '',$params = array()){
-		if(empty(self::$app[$class.'_'.$method]) && class_exists($class)){
+		if(!isset(self::$app[$class.'_'.$method]) && class_exists($class)){
 			Ref::methodInstace($class,$method);
 			if(Ref::isPublic() && Ref::isStatic()){
 				$class::$method();
@@ -63,27 +76,70 @@ class Container {
 	 * @author wave
 	 */
 	public static function set($key,$value,$params = array()){
-		if(empty(self::$app[$key]) && class_exists($value)){
+		if(isset(self::$app[$key])){
+			throw new XiaoBoException("该".$key."值已经注入过了,请先del,再来注入");
+		}
+		if(class_exists($value)){
 			Ref::classInstace($value);
 			self::$app[$key] = empty($params) ? Ref::instance() :  Ref::instanceArgs($params);
-		}else if(empty(self::$app[$key]) && is_callable($value)) {
-			self::$app[$key] = !empty($params) ? call_user_func_array($value,$params) :
-					call_user_func_array($value,array(self::$app));
-		}else if(empty(self::$app[$key]) && (is_string($value) || is_array($value)) ){
+		}else if(is_callable($value)) {
+			self::$app[$key] = !empty($params) ? call_user_func_array($value,$params) : 
+							call_user_func_array($value,array(self::$app));
+		}else if((is_string($value) || is_array($value)) ){
 			self::$app[$key] = $value;
 		}
 	}
-
+	
+	/**
+	 * 类的绑定
+	 * @param string $key 要绑定的类
+	 * @param string/array/object $value 值
+	 * @author wave
+	 */
+	public static function bind($key,$value){
+		if(!class_exists($key)){
+			throw new XiaoBoException("该".$key."类不存在,请先引入该类");
+		}
+		if(!isset(self::$bind[$key]) && $value != ''){
+			self::$bind[$key] = $value;
+		}
+	}
+	
+	/**
+	 * 初始化绑定参数
+	 * @param string $key 要初始化·绑定的类
+	 * @author wave
+	 */
+	public static function make($key){
+		if(!isset(self::$bind[$key])){
+			throw new XiaoBoException($key."未进行绑定,请先bind");
+		}
+		Ref::classInstace($key);
+		if(is_object(self::$bind[$key]) ){
+			self::$app[self::$bindPrefix.$key] = Ref::instance(self::$bind[$key]);
+		}else if (is_string(self::$bind[$key]) && class_exists(self::$bind[$key])){
+			self::instace(self::$bind[$key]);
+			self::$app[self::$bindPrefix.$key] = self::instance(self::get(self::$bind[$key]),'',array(),self::$bindPrefix);
+		}else if(is_array(self::$bind[$key])){
+			self::$app[self::$bindPrefix.$key] = Ref::instanceArgs(self::$bind[$key]);
+		}
+		return self::get($key);
+	}
+	
 	
 	/**
 	 * 获取实例化
 	 * @param string $key 要绑定的key
+	 * @return null/bool
 	 * @author wave
 	 */
 	public static function get($key){
-		if(isset(self::$app[$key])){
+		if(isset(self::$app[self::$bindPrefix.$key])){
+			return self::$app[self::$bindPrefix.$key];
+		}else if(isset(self::$app[$key])){
 			return self::$app[$key];
 		}
+		return NULL;
 	}
 
 
@@ -97,6 +153,7 @@ class Container {
 			unset(self::$app[$key]);
 		}
 	}
+	
 	
 	/**
 	 * 调用类方法
