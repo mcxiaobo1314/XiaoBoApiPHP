0ed947b499935a8555f112a85cc2c2a01c225792
xiaobo <mcxiaobo1314@qq.com>
2017-03-30 20:05:06 +0800

    优化路由代码

--- lib/conf/Route.php
+++ lib/conf/Route.php
@@ -11,7 +11,7 @@
  * 第四个参数:默认的方法名
  * 第五个参数:方法名里面的参数
  */
-RouteApi::aliasRoute('/index.html','home','test','aaa');
+//RouteApi::aliasRoute('/index.html','home','test','aaa');
 //一个{:number}绑定一个参数key,不能重复,访问则index.php/index/111/222.html 则会写入对应的参数
 RouteApi::aliasRoute('/index/{:1}/{:2}.html','home','test','sss', array('bc','ac'));
 ?>
